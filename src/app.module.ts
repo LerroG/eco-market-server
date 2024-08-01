eb83cbd63485e1a7b2d4e127a56eed74a3ecc62e
Ruslan <ssakoks@mail.ru>
2024-08-01 16:54:51 +0500

    add colors logic

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,8 +4,9 @@ import { ConfigModule } from '@nestjs/config'
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { StoreModule } from './store/store.module';
+import { ColorModule } from './color/color.module';
 
 @Module({
-	imports: [ConfigModule.forRoot(), PrismaModule, AuthModule, UserModule, StoreModule]
+	imports: [ConfigModule.forRoot(), PrismaModule, AuthModule, UserModule, StoreModule, ColorModule]
 })
 export class AppModule {}
