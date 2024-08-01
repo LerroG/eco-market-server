76a3ce978584508463a96a2c6a7152c7e08a9433
Ruslan <ssakoks@mail.ru>
2024-08-01 17:05:19 +0500

    add category logic

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,8 +5,9 @@ import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { StoreModule } from './store/store.module';
 import { ColorModule } from './color/color.module';
+import { CategoryModule } from './category/category.module';
 
 @Module({
-	imports: [ConfigModule.forRoot(), PrismaModule, AuthModule, UserModule, StoreModule, ColorModule]
+	imports: [ConfigModule.forRoot(), PrismaModule, AuthModule, UserModule, StoreModule, ColorModule, CategoryModule]
 })
 export class AppModule {}
